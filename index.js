48b6b0b5018593c58dcc63f33df8e1b10737d563
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2019-01-07 20:40:43 -0700

    make sure online

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-const ko = require('kodbm')
+//const Models = require('./lib/models')
+//const Routes = require('./lib/routes')
 
-const Models = require('./lib/models')
-const Routes = require('./lib/routes')
+//const prototype = {}
 
-const prototype = {}
+//db = Object.create(prototype)
 
-db = Object.create(prototype)
+//db.routes = {}
 
-db.routes = {}
+//db.models = new Proxy(Models(), {
+	//apply (target, thisArg, args) {
+		//target.apply(thisArg, args)
+		//for (const name in args[0]) {
+			//db.routes[name] = Routes(target[name])
+		//}
+		//return true
+	//}
+//})
 
-db.models = new Proxy(Models(), {
-	apply (target, thisArg, args) {
-		target.apply(thisArg, args)
-		for (const name in args[0]) {
-			db.routes[name] = Routes(target[name])
-		}
-		return true
+//db.ko = ko
+
+//module.exports = db
+
+const nekodb = require('nekodb')
+
+function APIRouter (Router) {
+	const o = Object.create(Router.prototype)
+	o.routes = {}
+	o.Router = Router
+	return o
+}
+
+APIRouter.prototype = Object.create(nekodb)
+
+APIRouter.prototype.models = function (schemas) {
+	for (const name in schemas) {
+		this.Model(name, schemas[name])
 	}
-})
+}
 
-db.ko = ko
+APIRouter.prototype.Model = function (name, schema) {
+	const Model = this.ko_models.Model(name, schema)
+	console.log(Model)
+	this.routes[name] = new Routes(Model, this.Router)
+	return Model
+}
 
-module.exports = db
+module.exports = APIRouter
